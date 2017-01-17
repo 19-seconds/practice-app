adc1e09ac00a0336fb0eb05a8d30cf64f2dbc7ca
Mark Richardson <markeban@yahoo.com>
2017-01-17 15:39:29 +0200

    finished exercise with bonus

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,22 @@ class PagesController < ApplicationController
     @your_fortune = fortunes.sample
     render "fortune.html"
   end
+
+  def lottery
+    @lottery_numbers = []
+    100.times do
+      @lottery_numbers << rand(10)
+    end
+
+    # @lottery_numbers = [rand(10), rand(10), rand(10), rand(10), rand(10)]
+  end
+
+  def ninety_nine_bottles
+    @lyrics = ""
+    count = 99
+    99.times do
+      @lyrics << "#{count} bottles of beer on the wall, #{count} bottles of beer"
+      count -=1
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "/fortune", to: 'pages#fortune'  
+  get "/fortune", to: 'pages#fortune'
+  get "/lottery", to: 'pages#lottery'
+  get "/bottles", to: 'pages#ninety_nine_bottles'
 end
